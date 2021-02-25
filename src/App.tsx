2125df91817ad4ae548ca5347b99c85174534504
Bradley Gaynor <BradleyGaynor@northwesternmutual.com>
2021-02-25 14:12:20 -0800

    Init tesseract.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { createWorker } from "tesseract.js";
+import "./App.css";
+
+const worker = createWorker({
+  logger: (m) => console.log(m),
+});
 
 function App() {
+
+  const doOCR = async () => {
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const {
+      data: { text },
+    } = await worker.recognize(
+      "https://tesseract.projectnaptha.com/img/eng_bw.png"
+    );
+    setOcr(text);
+  };
+  const [ocr, setOcr] = useState("Recognizing...");
+  useEffect(() => {
+    doOCR();
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src="https://tesseract.projectnaptha.com/img/eng_bw.png"
+          className="App-logo"
+          alt="logo"
+        />
+        <p>{ocr}</p>
       </header>
     </div>
   );
